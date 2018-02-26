bc60205c0f75ed2440f26f63842f1f522f13020b
MarikinPaulina <felicitaspura@gmail.com>
2018-02-26 07:15:22 +0100

    jafar2 niepelne

---
+++ DŻAFAR/lab2/example.java
@@ -0,0 +1,17 @@
+package lab2;
+
+import javax.swing.JFrame;
+
+public class example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame(); 
+		frame.setSize(640, 480); 
+		frame.setVisible(true);
+
+	}
+
+}
